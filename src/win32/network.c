6f59adfee1150d3e7cabb7e9e9a1224b2f83706b
Carson Howard <cjhoward92@gmail.com>
2018-03-31 19:30:11 -0700

    started work on windows networking

---
+++ src/win32/network.c
@@ -0,0 +1,53 @@
+#include "network.h"
+
+
+// Windows only
+static int init_wsa(WSADATA **out) {
+  *out = NULL;
+
+  int error;
+  WSADATA data;
+  if ((error = WSAStartup(MAKEWORD(2, 2), &data)) == 0) {
+    *out = &data;
+  }
+
+  return error;
+}
+
+static int init_sock(SOCKET **out) {
+  addrinfo *res = NULL, hints;
+
+  // memset on windows, I guess
+  ZeroMemory(&hints, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_protocol = IPPROTO_TCP;
+  hints.ai_flags = AI_PASSIVE;
+
+  int error = getaddrinfo(NULL, DEFAULT_SERVER_PORT, &hints, &res);
+  if (error) {
+    WSACleanup();
+    return error;
+  }
+
+  SOCKET sock = INVALID_SOCKET;
+  sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+  if (sock == INVALID_SOCKET) {
+    WSACleanup();
+    return 1; //TODO: some error here
+  }
+
+  error = bind(sock, res->ai_addr, (int)res->ai_addrlen);
+  
+  freeaddrinfo(res);
+
+  if (error) {
+    closesocket(sock);
+    WSACleanup();
+    return error;
+  }
+
+  *out = &sock;
+
+  return error;
+}
--- src/win32/network.h
+++ src/win32/network.h
@@ -1,11 +1,17 @@
 #ifndef __NETWORK_H__
 #define __NETWORK_H__
 
-#include <stdio.h>
 #include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
 #include <windows.h>
+#include <stdio.h>
 
 // https://www.binarytides.com/winsock-socket-programming-tutorial/
-#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "Ws2_32.lib")
+
+#define DEFAULT_SERVER_PORT "36000"
+
+typedef addrinfo addrinfo;
 
 #endif
